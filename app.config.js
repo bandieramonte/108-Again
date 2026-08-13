e4b855d3b4838548537854c43cd1ce681de77172
Gian Piero Bandieramonte <gian@bandieramonte.com>
2026-08-13 10:52:07 -0400

    Adjusted version and

--- app.config.js
+++ app.config.js
@@ -8,7 +8,7 @@ export default {
         name: "108 Again",
         slug: "108-again",
         scheme,
-        version: "2.0.1",
+        version: "2.0.2",
 
         orientation: "portrait",
         icon: "./assets/images/icon.png",
